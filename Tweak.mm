d6c4d4d8a519fdc73bae37642b3301d948938300
dtmpm3485 <visa200200200@gmail.com>
2026-01-30 07:47:57 +0900

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -3,6 +3,7 @@
 #import <mach-o/dyld.h>
 #import <mach-o/loader.h>
 #import <string.h>
+#import <mach/mach.h>
 
 #pragma mark - Memory Patch Core
 
@@ -34,11 +35,14 @@ static inline uintptr_t addr(intptr_t offset) {
 bool setvalue(intptr_t offset, int32_t value) {
     if (base == 0) return false;
     uintptr_t address = addr(offset);
+    
+    // 書き込み権限を付与
+    vm_protect(mach_task_self(), (vm_address_t)address, sizeof(value), FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    
     *((int32_t *)address) = value;
 
-    // デバッグ用: 書き込み後に値を確認
-    int32_t v = *((int32_t *)address);
-    NSLog(@"setvalue: addr=%p value=%d", (void *)address, v);
+    // デバッグ用
+    // NSLog(@"setvalue: addr=%p value=%d", (void *)address, value);
 
     return true;
 }
@@ -49,7 +53,7 @@ bool setvalue(intptr_t offset, int32_t value) {
 @property UIButton *floatingButton;
 @property UIView *overlay;
 @property UIView *menu;
-@property CGRect menuFrame; // メニュー位置保持
+@property CGRect menuFrame;
 
 @property UISwitch *toggleA;
 @property UISwitch *toggleB;
@@ -87,12 +91,9 @@ bool setvalue(intptr_t offset, int32_t value) {
     self.floatingButton.layer.cornerRadius = 28;
     self.floatingButton.backgroundColor = UIColor.systemGreenColor;
 
-    [self.floatingButton addTarget:self
-                            action:@selector(toggleMenu)
-                  forControlEvents:UIControlEventTouchUpInside];
+    [self.floatingButton addTarget:self action:@selector(toggleMenu) forControlEvents:UIControlEventTouchUpInside];
 
-    UIPanGestureRecognizer *pan =
-    [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(drag:)];
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(drag:)];
     [self.floatingButton addGestureRecognizer:pan];
 
     [w addSubview:self.floatingButton];
@@ -122,52 +123,58 @@ bool setvalue(intptr_t offset, int32_t value) {
     self.menu.backgroundColor = UIColor.whiteColor;
     self.menu.layer.cornerRadius = 14;
 
-    UIPanGestureRecognizer *pan =
-    [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(drag:)];
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(drag:)];
     [self.menu addGestureRecognizer:pan];
 
     UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(16, 12, 200, 24)];
     title.text = @"Mod Menu";
     title.font = [UIFont boldSystemFontOfSize:18];
+    [self.menu addSubview:title];
 
+    // --- 各項目のセットアップ (スイッチの状態を保持するため、存在しない場合のみ生成) ---
+    
+    // 猫缶
     UILabel *labelA = [[UILabel alloc] initWithFrame:CGRectMake(16, 60, 120, 24)];
     labelA.text = @"猫缶";
-    self.toggleA = [[UISwitch alloc] initWithFrame:CGRectMake(170, 56, 0, 0)];
+    [self.menu addSubview:labelA];
+    if (!self.toggleA) {
+        self.toggleA = [[UISwitch alloc] initWithFrame:CGRectMake(170, 56, 0, 0)];
+        [self.toggleA addTarget:self action:@selector(toggleChanged:) forControlEvents:UIControlEventValueChanged];
+    }
+    [self.menu addSubview:self.toggleA];
 
+    // XP
     UILabel *labelB = [[UILabel alloc] initWithFrame:CGRectMake(16, 110, 120, 24)];
     labelB.text = @"XP";
-    self.toggleB = [[UISwitch alloc] initWithFrame:CGRectMake(170, 106, 0, 0)];
+    [self.menu addSubview:labelB];
+    if (!self.toggleB) {
+        self.toggleB = [[UISwitch alloc] initWithFrame:CGRectMake(170, 106, 0, 0)];
+        [self.toggleB addTarget:self action:@selector(toggleChanged:) forControlEvents:UIControlEventValueChanged];
+    }
+    [self.menu addSubview:self.toggleB];
 
+    // NP
     UILabel *labelC = [[UILabel alloc] initWithFrame:CGRectMake(16, 160, 120, 24)];
     labelC.text = @"NP";
-    self.toggleC = [[UISwitch alloc] initWithFrame:CGRectMake(170, 156, 0, 0)];
-
-    [self.toggleA addTarget:self action:@selector(toggleChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.toggleB addTarget:self action:@selector(toggleChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.toggleC addTarget:self action:@selector(toggleChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.menu addSubview:labelC];
+    if (!self.toggleC) {
+        self.toggleC = [[UISwitch alloc] initWithFrame:CGRectMake(170, 156, 0, 0)];
+        [self.toggleC addTarget:self action:@selector(toggleChanged:) forControlEvents:UIControlEventValueChanged];
+    }
+    [self.menu addSubview:self.toggleC];
 
     UIButton *close = [UIButton buttonWithType:UIButtonTypeSystem];
     close.frame = CGRectMake(150, 250, 80, 30);
     [close setTitle:@"Close" forState:UIControlStateNormal];
-    [close addTarget:self action:@selector(hideMenu)
-            forControlEvents:UIControlEventTouchUpInside];
-
-    [self.menu addSubview:title];
-    [self.menu addSubview:labelA];
-    [self.menu addSubview:self.toggleA];
-    [self.menu addSubview:labelB];
-    [self.menu addSubview:self.toggleB];
-    [self.menu addSubview:labelC];
-    [self.menu addSubview:self.toggleC];
+    [close addTarget:self action:@selector(hideMenu) forControlEvents:UIControlEventTouchUpInside];
     [self.menu addSubview:close];
 
     [self.overlay addSubview:self.menu];
 }
 
 - (void)hideMenu {
-    self.menuFrame = self.menu.frame; // 閉じる前に位置を保持
-    [self.timer invalidate];
-    self.timer = nil;
+    self.menuFrame = self.menu.frame;
+    // スイッチ自体は破棄せず、オーバーレイだけ消す
     [self.overlay removeFromSuperview];
     self.overlay = nil;
 }
@@ -186,7 +193,7 @@ bool setvalue(intptr_t offset, int32_t value) {
 - (void)toggleChanged:(UISwitch *)sw {
     if (self.toggleA.isOn || self.toggleB.isOn || self.toggleC.isOn) {
         if (!self.timer) {
-            self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1
+            self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5
                                                           target:self
                                                         selector:@selector(applyPatch)
                                                         userInfo:nil
@@ -199,17 +206,16 @@ bool setvalue(intptr_t offset, int32_t value) {
 }
 
 - (void)applyPatch {
-    if (self.toggleA.isOn) {
+    // スイッチの状態に応じて書き込み
+    if (self.toggleA && self.toggleA.isOn) {
         setvalue(addr1, 58999);
         setvalue(addr1 + 4, 0);
     }
-
-    if (self.toggleB.isOn) {
+    if (self.toggleB && self.toggleB.isOn) {
         setvalue(addr2, 999);
         setvalue(addr2 + 4, 0);
     }
-
-    if (self.toggleC.isOn) {
+    if (self.toggleC && self.toggleC.isOn) {
         setvalue(addr3, 1);
         setvalue(addr3 + 4, 0);
     }
@@ -224,10 +230,11 @@ static void entry() {
     const char *moduleName = "jp.co.ponos.battlecats";
     uintptr_t start = getModuleBase(moduleName);
     if (start) {
+        // iGG等で計算したオフセットを足す
+        // もし値が変わらない場合、ここを base = *(uintptr_t *)(start + 0x1976D60); に変えてみてください
         base = start + 0x1976D60;
     }
     dispatch_async(dispatch_get_main_queue(), ^{
         [[ModMenuManager shared] showFloatingButton];
     });
 }
-// by 制作 ChatGPT
